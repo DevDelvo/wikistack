79defa0449549f2b60fd7e51afb80ab182f677d0
Kevin Delvo <kevinbdelvo@gmail.com>
2018-04-27 12:12:40 -0400

    fuck windows

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const layout = require("./views/layout");
+const db = require("./models");
 
 const app = express();
 
 app.use(morgan("dev"));
 // app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+})
 
 // main route
 app.get("/", (req, res, next) => {
